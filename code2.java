115a7c4f50d554f70c2c1dde442add26916f2d69
Thach Hoang Son <son.thach011011@gmail.com>
2021-09-23 22:26:51 +0700

    final reivew code2

--- code2.java
+++ code2.java
@@ -1,67 +1,75 @@
 import java.util.HashMap;
 
-public class HSSearch {
-        private static HashMap<Character, Integer> shiftValues;
-        private static Integer PatternLength, Comparisons = 0;
+public class BoyerMooreSearch {
+        private HashMap<Character, Integer> shiftValues;
+        private Integer patternLength, comparisonCount = 0;
 
-        private static int findFirst(String p, String te) {
+        private int findFirst(String p, String te) {
                 return firstOccurrence(p, te, true);
         }
 
-        public static Integer getLastComparisons() {
-                return this.comparisons;
+        public Integer getLastComparisonCount() {
+                return this.comparisonCount;
         }
 
-        public static int findFirstInsensitive(String ptern, String tx) {
+        public int findFirstInsensitive(String ptern, String tx) {
                 return firstOccurrence(ptern, tx, false);
         }
 
-        private static int firstOccurrence(String pa, String te, boolean cs) {
-                shiftValues = calcShiftValues(p);
-                int comparision = 0;
-                int teidx = pa.length() - 1;
-                while (teidx < te.length()) {
-                        int i = pa.length();
-                        i--;
+        /**
+         * 
+         * @param pa The pattern that being checked if exists in the text
+         * @param te The text which is checked by the pattern
+         * @param cs The boolean showing the search algorithm is case sensitive or not
+         * @return The first occurence index if found, -1 if not found pattern
+         */
+        private int firstOccurrence(String pa, String te, boolean cs) {
+                shiftValues = calcShiftValues(pa);
+                int teIdx = pa.length() - 1;
+                while (teIdx < te.length()) {
+                        int i = pa.length() - 1;
                         while (!(i < 0)) {
-                                comparisons++;
+                                comparisonCount++;
                                 char patternChar = pa.charAt(i);
-                                char textChar = te.charAt((teidx + i) - (pa.length() - 1));
+                                char textChar = te.charAt((teIdx + i) - (pa.length() - 1));
                                 if (!charEquals(patternChar, textChar, cs)) {
-                                        teidx += getShiftValue(te.charAt(teidx));
+                                        teIdx += getShiftValue(textChar);
                                         break;
                                 }
                                 i--;
                         }
                         if (i == -1) {
-                                return teidx - pa.length() + 1;
+                                return teIdx - pa.length() + 1;
                         }
                 }
                 return -1;
         }
 
-        private static boolean charEquals(char c1, char c2, boolean cs) {
+        private boolean charEquals(char c1, char c2, boolean cs) {
                 if (cs) {
                         return c1 == c2;
                 }
                 return Character.toLowerCase(c1) == Character.toLowerCase(c2);
         }
 
-        private static HashMap<Character, Integer> calcShiftValues(String pa) {
+        /**
+         * @param pa A string that used to produce table shiftValues
+         * @return it return tables represent the position of pattern from right -> left
+         *         except the last character
+         */
+        private HashMap<Character, Integer> calcShiftValues(String pa) {
                 patternLength = pa.length();
                 HashMap<Character, Integer> table = new HashMap<>();
                 for (int i = pa.length() - 2; i >= 0; i--) {
                         char c = pa.charAt(i);
-                        int finalI = i;
-                        table.computeIfAbsent(c, k -> pa.length() - 1 - finalI);
+                        table.computeIfAbsent(c, k -> pa.length() - 1 - i);
                 }
                 return table;
         }
 
-        private static Integer getShiftValue(char c) {
-                char localCahr = c;
+        private Integer getShiftValue(char c) {
                 if (shiftValues.get(c) != null) {
-                        return shiftValues.get(localCahr);
+                        return shiftValues.get(c);
                 } else {
                         return patternLength;
                 }
